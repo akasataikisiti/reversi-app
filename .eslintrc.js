f90ef4172552f0104ab9ffb8274dc77c4a595a27
akasataikisiti <nama0you0chu@gmail.com>
2023-02-24 22:15:59 +0900

    エクスプレスサーバ立ち上げ

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,19 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es2021: true
   },
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
-  ],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module",
+    ecmaVersion: './tsconfig.json',
+    sourceType: 'module'
   },
-  plugins: ["prettier", "@typescript-eslint"],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/ban-ts-ignore": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-  },
-};
+    '@typescript-eslint/no-explicit-any': 1,
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/no-unused-vars': 'off'
+  }
+}
