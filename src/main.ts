9a343452038b6730b156a786cdc2bb32bf333acd
akasataikisiti <nama0you0chu@gmail.com>
2023-02-26 23:31:27 +0900

    ドメインモデルパターン　クラス作成

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
 import 'express-async-errors'
-import { GameGateway } from './dataaccess/gameGateway'
-import { TurnGateway } from './dataaccess/turnGateway'
-import { MoveGateway } from './dataaccess/moveGateway'
-import { SquareGateway } from './dataaccess/squareGateway'
 import { gameRouter } from './presentation/gameRouter'
 import { turnRouter } from './presentation/turnRouter'
 
@@ -15,11 +11,6 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.static('static', { extensions: ['html'] }))
 app.use(express.json())
-
-const gameGateway = new GameGateway()
-const turnGateway = new TurnGateway()
-const moveGateway = new MoveGateway()
-const squareGateway = new SquareGateway()
 
 app.use(gameRouter)
 app.use(turnRouter)
