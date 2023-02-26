d0f8395eb4827a3ab8b25ba9215d4d9927ac4bad
akasataikisiti <nama0you0chu@gmail.com>
2023-02-26 19:34:27 +0900

    プレゼンテーション層作成 2
    turnRouter作成

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
 import 'express-async-errors'
-import mysql from 'mysql2/promise'
 import { GameGateway } from './dataaccess/gameGateway'
 import { TurnGateway } from './dataaccess/turnGateway'
 import { MoveGateway } from './dataaccess/moveGateway'
 import { SquareGateway } from './dataaccess/squareGateway'
 import { gameRouter } from './presentation/gameRouter'
-
-const EMPTY = 0
-const DARK = 1
-const LIGHT = 2
-
-const INITIAL_BOARD = [
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-]
+import { turnRouter } from './presentation/turnRouter'
 
 const PORT = 3000
 
@@ -47,105 +32,7 @@ app.get('/api/error', async (req, res) => {
 })
 
 app.use(gameRouter)
-
-app.get('/api/games/latest/turns/:turnCount', async (req, res) => {
-  const turnCount = parseInt(req.params.turnCount)
-
-  const conn = await connectMySQL()
-  try {
-    const gameRecord = await gameGateway.findLatest(conn)
-
-    if (!gameRecord) {
-      throw new Error('Latest game not found')
-    }
-
-    const turnRecord = await turnGateway.findForGameIdAndTurnCount(
-      conn,
-      gameRecord.id,
-      turnCount
-    )
-
-    if (!turnRecord) throw new Error('Specified turn not found')
-
-    const squareRecords = await squareGateway.findForTurnId(conn, turnRecord.id)
-    const board = Array.from(Array(8)).map(() => Array.from(Array(8)))
-    squareRecords.forEach((s) => {
-      board[s.y][s.x] = s.disc
-    })
-
-    const responseBody = {
-      turnCount,
-      board,
-      nextDisc: turnRecord.nextDisc,
-      // TODO 決着がついている場合、game_resultsテーブルから取得する
-      winnerDisc: null,
-    }
-    res.json(responseBody)
-  } finally {
-    await conn.end()
-  }
-})
-
-app.post('/api/games/latest/turns', async (req, res) => {
-  const turnCount = parseInt(req.body.turnCount)
-  const disc = parseInt(req.body.move.disc)
-  const x = parseInt(req.body.move.x)
-  const y = parseInt(req.body.move.y)
-
-  const conn = await connectMySQL()
-  try {
-    // 1つ前のターンを取得する
-    const gameRecord = await gameGateway.findLatest(conn)
-    if (!gameRecord) throw new Error('Latest game not found')
-
-    const previousTurnCount = turnCount - 1
-
-    const previousTurnRecord = await turnGateway.findForGameIdAndTurnCount(
-      conn,
-      gameRecord.id,
-      previousTurnCount
-    )
-
-    if (!previousTurnRecord) throw new Error('Specified turn not found')
-
-    const squareRecords = await squareGateway.findForTurnId(
-      conn,
-      previousTurnRecord.id
-    )
-    const board = Array.from(Array(8)).map(() => Array.from(Array(8)))
-    squareRecords.forEach((s) => {
-      board[s.y][s.x] = s.disc
-    })
-
-    // 盤面に置けるかチェック
-
-    // 石を置く
-    board[y][x] = disc
-    console.log(board)
-
-    // ひっくり返す
-
-    // ターンを保存する
-    const nextDisc = disc === DARK ? LIGHT : DARK
-    const now = new Date()
-    const turnRecord = await turnGateway.insert(
-      conn,
-      gameRecord.id,
-      turnCount,
-      nextDisc,
-      now
-    )
-
-    await squareGateway.insertAll(conn, turnRecord.id, board)
-
-    await moveGateway.insert(conn, turnRecord.id, disc, x, y)
-
-    await conn.commit()
-  } finally {
-    await conn.end()
-  }
-  res.status(201).end()
-})
+app.use(turnRouter)
 
 app.use(errorHandler)
 
@@ -162,14 +49,5 @@ function errorHandler(
   console.log('Unexpected error occurrd', err)
   res.status(500).send({
     message: 'Unexpected error occurred',
-  })
-}
-
-async function connectMySQL() {
-  return await mysql.createConnection({
-    host: 'localhost',
-    database: 'reversi',
-    user: 'reversi',
-    password: 'password',
   })
 }
