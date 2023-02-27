fed5b4a9186babfc20453bcb2d3d16789d008d1f
akasataikisiti <nama0you0chu@gmail.com>
2023-02-28 07:47:41 +0900

    ひっくり返せる点をリストアップする処理の作成
    一旦上方向分だけ作成

--- src/domain/turn/board.ts
+++ src/domain/turn/board.ts
@@ -1,16 +1,20 @@
-import { Disc } from './disc'
+import { Disc, isOppositeDisc } from './disc'
 import { Move } from './move'
 import { Point } from './point'
 
 export class Board {
-  constructor(private _discs: Disc[][]) {}
+  private _walledDiscs: Disc[][]
+
+  constructor(private _discs: Disc[][]) {
+    this._walledDiscs = this.walledDiscs()
+  }
 
   place(move: Move): Board {
     // からのマス目ではない場合、置くことはできない
     if (this._discs[move.point.y][move.point.x] !== Disc.Empty)
       throw new Error('Selected point is not empty')
     // ひっくり返せる点をリストアップ
-    const flipPoints = this.listFlipPoints()
+    const flipPoints = this.listFlipPoints(move)
 
     // ひっくり返せる点がない場合置くことはできない
     if (flipPoints.length === 0) throw new Error('Flip points is empty')
@@ -26,8 +30,46 @@ export class Board {
     return new Board(newDiscs)
   }
 
-  private listFlipPoints(): Point[] {
-    return [new Point(0, 0)]
+  private listFlipPoints(move: Move): Point[] {
+    const flipPoints: Point[] = []
+
+    const walledX = move.point.x + 1
+    const walledY = move.point.y + 1
+
+    // 上
+    const flipCandidate: Point[] = []
+
+    // 一つ動いた位置から開始
+    const cursorX = walledX
+    let cursorY = walledY - 1
+
+    while (isOppositeDisc(move.disc, this._walledDiscs[cursorY][cursorX])) {
+      // 番兵を考慮して-1する
+      flipCandidate.push(new Point(cursorX - 1, cursorY - 1))
+      cursorY--
+      // 次の手が同じ色の石なら、ひっくり返す石が確定
+      if (move.disc === this._walledDiscs[cursorY][cursorX]) {
+        flipPoints.push(...flipCandidate)
+        break
+      }
+    }
+    return flipPoints
+  }
+
+  private walledDiscs(): Disc[][] {
+    const walled: Disc[][] = []
+
+    const topAndBottomWall = Array(this._discs[0].length + 2).fill(Disc.Wall)
+    walled.push(topAndBottomWall)
+
+    this._discs.forEach((line) => {
+      const walledLine = [Disc.Wall, ...line, Disc.Wall]
+      walled.push(walledLine)
+    })
+
+    walled.push(topAndBottomWall)
+
+    return walled
   }
 
   get discs() {
