7e73caea2da2f63f7002b2f4837d89c6bedf50ae
akasataikisiti <nama0you0chu@gmail.com>
2023-03-01 04:04:28 +0900

    次の石を判定する処理実装 boardの判定関数作成

--- src/domain/model/turn/board.ts
+++ src/domain/model/turn/board.ts
@@ -87,7 +87,27 @@ export class Board {
   }
 
   existValidMove(disc: Disc): boolean {
-    return true
+    for (let y = 0; y < this._discs.length; y++) {
+      const line = this._discs[y]
+
+      for (let x = 0; x < line.length; x++) {
+        const discOnBoard = line[x]
+
+        // 空ではない点は無視
+        if (discOnBoard !== Disc.Empty) {
+          continue
+        }
+
+        const move = new Move(disc, new Point(x, y))
+        const flipPoints = this.listFlipPoints(move)
+
+        // ひっくり返せる点がある場合、おける場所がある
+        if (flipPoints.length !== 0) {
+          return true
+        }
+      }
+    }
+    return false
   }
 
   private walledDiscs(): Disc[][] {
