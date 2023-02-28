5e81b2ac0fbdd5880af719ac840eafa498c21248
akasataikisiti <nama0you0chu@gmail.com>
2023-02-28 10:22:45 +0900

    ドメイン層のエラーハンドリング実装
    domeinディレクトリ以下にmodelディレクトリとerrorディレクトリを作成

--- src/domain/model/turn/board.ts
+++ src/domain/model/turn/board.ts
@@ -1,3 +1,4 @@
+import { DomainError } from '../../error/domainError'
 import { Disc, isOppositeDisc } from './disc'
 import { Move } from './move'
 import { Point } from './point'
@@ -12,12 +13,16 @@ export class Board {
   place(move: Move): Board {
     // からのマス目ではない場合、置くことはできない
     if (this._discs[move.point.y][move.point.x] !== Disc.Empty)
-      throw new Error('Selected point is not empty')
+      throw new DomainError(
+        'SelectedPointIsNotEmpty',
+        'Selected point is not empty'
+      )
     // ひっくり返せる点をリストアップ
     const flipPoints = this.listFlipPoints(move)
 
     // ひっくり返せる点がない場合置くことはできない
-    if (flipPoints.length === 0) throw new Error('Flip points is empty')
+    if (flipPoints.length === 0)
+      throw new DomainError('FlipPointsIsEmpty', 'Flip points is empty')
 
     //  盤面をコピー
     const newDiscs = this._discs.map((line) => {
--- src/domain/model/turn/turnRepository.ts
+++ src/domain/model/turn/turnRepository.ts
@@ -1,7 +1,8 @@
 import mysql from 'mysql2/promise'
-import { MoveGateway } from '../../infrastructure/moveGateway'
-import { SquareGateway } from '../../infrastructure/squareGateway'
-import { TurnGateway } from '../../infrastructure/turnGateway'
+import { MoveGateway } from '../../../infrastructure/moveGateway'
+import { SquareGateway } from '../../../infrastructure/squareGateway'
+import { TurnGateway } from '../../../infrastructure/turnGateway'
+import { DomainError } from '../../error/domainError'
 import { Board } from './board'
 import { toDisc } from './disc'
 import { Move } from './move'
@@ -25,7 +26,8 @@ export class TurnRepository {
       turnCount
     )
 
-    if (!turnRecord) throw new Error('Specified turn not found')
+    if (!turnRecord)
+      throw new DomainError('SpecifiedTurnNotFound', 'Specified turn not found')
 
     const squareRecords = await squareGateway.findForTurnId(conn, turnRecord.id)
     const board = Array.from(Array(8)).map(() => Array.from(Array(8)))
