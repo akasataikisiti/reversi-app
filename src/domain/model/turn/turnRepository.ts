a6f2d67b66eea9aec3a75e28878a423d7b70988a
akasataikisiti <nama0you0chu@gmail.com>
2023-03-02 00:20:11 +0900

    Repogisoryインターフェースと実装の分離
    インターフェース作成してフォルダを分ける

--- src/domain/model/turn/turnRepository.ts
+++ src/domain/model/turn/turnRepository.ts
@@ -1,81 +1,13 @@
 import mysql from 'mysql2/promise'
-import { MoveGateway } from '../../../infrastructure/moveGateway'
-import { SquareGateway } from '../../../infrastructure/squareGateway'
-import { TurnGateway } from '../../../infrastructure/turnGateway'
-import { DomainError } from '../../error/domainError'
-import { Board } from './board'
-import { toDisc } from './disc'
-import { Move } from './move'
-import { Point } from './point'
 import { Turn } from './turn'
 
-const turnGateway = new TurnGateway()
-const squareGateway = new SquareGateway()
-const moveGateway = new MoveGateway()
-
-export class TurnRepository {
+export interface TurnRepository {
   // 指定したターンを取得できるようにする (元は一つ前のターン取得)
-  async findForGameIdAndTurnCount(
+  findForGameIdAndTurnCount(
     conn: mysql.Connection,
     gameId: number,
     turnCount: number
-  ): Promise<Turn> {
-    const turnRecord = await turnGateway.findForGameIdAndTurnCount(
-      conn,
-      gameId,
-      turnCount
-    )
+  ): Promise<Turn>
 
-    if (!turnRecord)
-      throw new DomainError('SpecifiedTurnNotFound', 'Specified turn not found')
-
-    const squareRecords = await squareGateway.findForTurnId(conn, turnRecord.id)
-    const board = Array.from(Array(8)).map(() => Array.from(Array(8)))
-    squareRecords.forEach((s) => {
-      board[s.y][s.x] = s.disc
-    })
-
-    // 指定したターンのmoveを取得
-    const moveRecord = await moveGateway.findForTurnId(conn, turnRecord.id)
-    let move: Move | undefined = undefined
-    if (moveRecord) {
-      move = new Move(
-        toDisc(moveRecord.disc),
-        new Point(moveRecord.x, moveRecord.y)
-      )
-    }
-
-    const nextDisc =
-      turnRecord.nextDisc === null ? undefined : toDisc(turnRecord.nextDisc)
-
-    return new Turn(
-      gameId,
-      turnCount,
-      nextDisc,
-      move,
-      new Board(board),
-      turnRecord.endAt
-    )
-  }
-
-  async save(conn: mysql.Connection, turn: Turn) {
-    const turnRecord = await turnGateway.insert(
-      conn,
-      turn.gameId,
-      turn.turnCount,
-      turn.nextDisc,
-      turn.endAt
-    )
-    await squareGateway.insertAll(conn, turnRecord.id, turn.board.discs)
-
-    if (turn.move) {
-      await moveGateway.insert(
-        conn,
-        turnRecord.id,
-        turn.move.disc,
-        turn.move.point.x,
-        turn.move.point.y
-      )
-    }
-  }
+  save(conn: mysql.Connection, turn: Turn): Promise<void>
 }
